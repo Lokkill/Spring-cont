3b8ee1139e817cd7dbe1a045f22f0eb77ee6e58c
Andrey Kataev <corvolk@yandex.ru>
2021-04-09 11:08:00 +0300

    Complete HW

---
+++ src/main/java/ru/geekbrains/Cart.java
@@ -0,0 +1,45 @@
+package ru.geekbrains;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Stream;
+
+@Component
+public class Cart {
+    @Autowired
+    private ProductRepository repository;
+
+    private Map<Long, Product> userCart;
+
+    private AtomicLong id = new AtomicLong(0);
+
+    @PostConstruct
+    public void init() {
+        userCart = new HashMap<>();
+    }
+
+    public void addToCart(Long idProduct) {
+        try {
+            userCart.put(id.incrementAndGet(), repository.getProductById(idProduct));
+        } catch (Exception e) {
+            System.out.println("Продукта с таким id - нет, попробуйте снова");
+        }
+    }
+
+    public void removeFromCart(Long id) {
+        try {
+            userCart.remove(id);
+        } catch (Exception e) {
+            System.out.println("Продукта с таким id - нет, попробуйте снова");
+        }
+    }
+
+    public void getCartList() {
+        Stream.of(userCart).forEach(System.out::println);
+    }
+}
--- src/main/java/ru/geekbrains/Main.java
+++ src/main/java/ru/geekbrains/Main.java
@@ -1,4 +1,54 @@
 package ru.geekbrains;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("ru.geekbrains");
+        ProductRepository repository = context.getBean("productRepository", ProductRepository.class);
+        Cart cart = context.getBean("cart", Cart.class);
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            System.out.println("-----------------------");
+            System.out.println("Введите команду: \n\t" +
+                    "1. Получить список товаров \n\t" +
+                    "2. Получить товар по id \n\t" +
+                    "3. Вывести корзину \n\t" +
+                    "4. Добавить товар в корзину (по id) \n\t" +
+                    "5. Удалить товар из корзины (по id) \n\t" +
+                    "6. Выход");
+            switch (scanner.nextLine()){
+                case "1":
+                    repository.getProductsList();
+                    break;
+                case "2":
+                    repository.getProductsList(checkCorrectValue(scanner));
+                    break;
+                case "3":
+                    cart.getCartList();
+                    break;
+                case "4":
+                    cart.addToCart(checkCorrectValue(scanner));
+                    break;
+                case "5":
+                    cart.removeFromCart(checkCorrectValue(scanner));
+                    break;
+                case "6":
+                    context.close();
+                    System.exit(-1);
+            }
+        }
+    }
+
+    public static Long checkCorrectValue(Scanner scanner){
+        try {
+            System.out.println("Введите число: ");
+            return Long.parseLong(scanner.nextLine());
+        } catch (Exception e) {
+            System.out.println("Некорректно введено значение, попробуйте снова");
+            return checkCorrectValue(scanner);
+        }
+    }
 }
